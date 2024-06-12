1ca7549a3fbd6d9ce2432cb118b37b14db8ccd6a
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2024-06-12 16:47:04 -0300

    Player se movendo

--- Hello World/player.gd
+++ Hello World/player.gd
@@ -1,5 +1,5 @@
 extends Sprite2D
-
+@export var speed = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +9,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	position += input * speed * delta
 	pass
