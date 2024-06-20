08ac724c03b98e1f0f225f467601804f16ceed8e
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2024-06-20 10:51:07 -0300

    variavel no inspector

--- Industrial Plataformer/scripts/player.gd
+++ Industrial Plataformer/scripts/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+@export var speed = 300.0
 
 
 func _physics_process(delta):
 	
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * SPEED
+	velocity = direction * speed * 100
 	move_and_slide()
