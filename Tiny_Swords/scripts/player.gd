57c4e992b0545c3df21ebb69c0cec7efa84b41be
Isabelle Reny <isabelleribaldo@hotmail.com>
2024-07-01 15:29:51 -0300

    ataque do personagem

--- Tiny_Swords/scripts/player.gd
+++ Tiny_Swords/scripts/player.gd
@@ -2,9 +2,23 @@ extends CharacterBody2D
 
 @onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 @onready var sprite2D: Sprite2D = $Sprite2D
-var isRunning: bool = false
 @export var speed: float = 1.0
 @export_range(0,1) var my_lerp: float = 0.2
+
+var isRunning: bool = false
+var isAttacking : bool = false
+var attackCooldown : float = 0.0
+
+
+func _process(delta):
+	#atualizar temporizador do ataque
+	if isAttacking:
+		attackCooldown -= delta
+		if attackCooldown <= 0:
+			isAttacking = false
+			isRunning = false
+			animationPlayer.play("idle")
+	pass
 
 #movimentar o jogador 
 func _physics_process(delta):
@@ -20,6 +34,8 @@ func _physics_process(delta):
 	
 	# velocidade
 	var new_velocity = inputV * speed * 100
+	if isAttacking:
+		new_velocity = new_velocity * 0.25 
 	velocity = lerp(velocity, new_velocity, my_lerp)
 	#move o corpo baseado na velocidade
 	move_and_slide()
@@ -29,11 +45,12 @@ func _physics_process(delta):
 	isRunning = not inputV.is_zero_approx()
 	
 	#tocar animação
-	if wasRunning != isRunning:
-		if isRunning: 
-			animationPlayer.play("run")
-		else:
-			animationPlayer.play("idle")
+	if not isAttacking: 
+		if wasRunning != isRunning:
+			if isRunning: 
+				animationPlayer.play("run")
+			else:
+				animationPlayer.play("idle")
 	
 	#trocar lado da sprite 
 	if inputV.x > 0:
@@ -42,5 +59,25 @@ func _physics_process(delta):
 	elif inputV.x < 0:
 		#marcar flip_h do Sprite2D
 		sprite2D.flip_h = true
-	pass
 	
+	#sistema de ataque
+	if Input.is_action_just_pressed("attack"):
+		attack()
+	pass
+
+#ataque do jogador
+func attack():
+	if isAttacking:
+		return
+	
+	#attack // attack_side_2
+	
+	animationPlayer.play("attack")
+	
+	#configurar temporizador
+	attackCooldown = 0.6
+	#marcar ataque
+	isAttacking = true
+	
+	
+	pass
