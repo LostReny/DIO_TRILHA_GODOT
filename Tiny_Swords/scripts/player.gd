9020de05f8838d43f2b18adf1ab12e978f468435
Isabelle Reny <isabelleribaldo@hotmail.com>
2024-07-01 15:44:20 -0300

    melhorando script

--- Tiny_Swords/scripts/player.gd
+++ Tiny_Swords/scripts/player.gd
@@ -5,32 +5,29 @@ extends CharacterBody2D
 @export var speed: float = 1.0
 @export_range(0,1) var my_lerp: float = 0.2
 
+var inputV : Vector2 = Vector2(0,0)
 var isRunning: bool = false
+var wasRunning: bool = false
 var isAttacking : bool = false
 var attackCooldown : float = 0.0
 
 
 func _process(delta):
-	#atualizar temporizador do ataque
-	if isAttacking:
-		attackCooldown -= delta
-		if attackCooldown <= 0:
-			isAttacking = false
-			isRunning = false
-			animationPlayer.play("idle")
+	
+	_readInput()
+	
+	_attack_cooldown(delta)
+	
+	_animation()
+	
+	#sistema de ataque
+	if Input.is_action_just_pressed("attack"):
+		attack()
+	
 	pass
 
 #movimentar o jogador 
 func _physics_process(delta):
-	#input_vector
-	var inputV = Input.get_vector("move_left","move_right","move_up","move_down")
-	
-	#apagar dead_zone inputV
-	var deadZone = 0.15
-	if abs(inputV.x) < 0.15: 
-		inputV.x = 0.0
-	if abs(inputV.y) < 0.15: 
-		inputV.y = 0.0
 	
 	# velocidade
 	var new_velocity = inputV * speed * 100
@@ -39,10 +36,10 @@ func _physics_process(delta):
 	velocity = lerp(velocity, new_velocity, my_lerp)
 	#move o corpo baseado na velocidade
 	move_and_slide()
-	
-	#atualizar isRunning
-	var wasRunning = isRunning
-	isRunning = not inputV.is_zero_approx()
+	pass
+
+#animação do jogador
+func _animation():
 	
 	#tocar animação
 	if not isAttacking: 
@@ -59,10 +56,37 @@ func _physics_process(delta):
 	elif inputV.x < 0:
 		#marcar flip_h do Sprite2D
 		sprite2D.flip_h = true
+	pass
+
+#input do jogador 
+func _readInput():
 	
-	#sistema de ataque
-	if Input.is_action_just_pressed("attack"):
-		attack()
+	#input Vector
+	inputV = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	#apagar dead_zone inputV
+	var deadZone = 0.15
+	if abs(inputV.x) < 0.15: 
+		inputV.x = 0.0
+	if abs(inputV.y) < 0.15: 
+		inputV.y = 0.0
+	
+	#atualizar isRunning
+	wasRunning = isRunning
+	isRunning = not inputV.is_zero_approx()
+	
+	pass
+
+#ataque cooldown
+func _attack_cooldown(delta: float):
+	#atualizar temporizador do ataque
+	if isAttacking:
+		attackCooldown -= delta
+		if attackCooldown <= 0:
+			isAttacking = false
+			isRunning = false
+			animationPlayer.play("idle")
+	
 	pass
 
 #ataque do jogador
@@ -78,6 +102,4 @@ func attack():
 	attackCooldown = 0.6
 	#marcar ataque
 	isAttacking = true
-	
-	
 	pass
