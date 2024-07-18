11b6a4cbb7503b05e9573b58c1e5983edb616325
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2024-07-18 17:14:45 -0300

    mob_spawner

--- Tiny_Swords/scripts/systems/mob_spanner.gd
+++ Tiny_Swords/scripts/systems/mob_spanner.gd
@@ -1,19 +1,37 @@
 extends Node2D
 
 @export var creatures : Array[PackedScene]
+@export var mobs_per_minute: float = 60.0
+
 
 @onready var path_follow_2d: PathFollow2D = %PathFollow2D
-
+var cooldown : float = 0.0
 
 func _process(delta):
-	#frequencia de monstros por minuto 
+	
 	#temporizador (cooldown)
+	cooldown -= delta
+	if cooldown > 0: return
+	
+	#frequencia de monstros por minuto
+	var interval = 60.0 / mobs_per_minute
+	cooldown = interval
+	
 	#instanciar uma criatura aleatoria
+	# pegar criatura aleatoria / ponto aleatoria
+	#instanciar cena/colocar na posição
+	
+	var index = randi_range(0,creatures.size() - 1)
+	var creature_scene = creatures[index]
+	var point = get_point()
+	var creature = creature_scene.instantiate()
+	creature.global_position = get_point()
+	get_parent().add_child(creature)
 	
 	pass
 
 
 func get_point() -> Vector2:
 	path_follow_2d.progress_ratio = randf()
-	return path_follow_2d.position
+	return path_follow_2d.global_position
 	pass
